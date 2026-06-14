d65414f454301aca353818b8da1a1e8564f7fd38
Puxuan W. <puxuan.wang@tum.de>
2026-06-14 19:06:16 +0200

    use webp instead of png

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 const SITE_CONFIG = {
     logoPath: 'resources/logo-radiance.webp',
     featureImages: {
-        rt: 'https://image.puxuan.cc/PicGo/woods.png',
-        interact: 'https://image.puxuan.cc/PicGo/gloom.png',
-        upscale: 'https://image.puxuan.cc/PicGo/underground.png',
-        pbr: 'https://image.puxuan.cc/PicGo/2026-02-09_00.45.30.png',
-        pipeline: 'https://image.puxuan.cc/PicGo/pipeline.png'
+        rt: 'https://image.puxuan.cc/PicGo/woods.webp',
+        interact: 'https://image.puxuan.cc/PicGo/gloom.webp',
+        upscale: 'https://image.puxuan.cc/PicGo/underground.webp',
+        pbr: 'https://image.puxuan.cc/PicGo/pbr.webp',
+        pipeline: 'https://image.puxuan.cc/PicGo/pipeline.webp'
     }
 };
